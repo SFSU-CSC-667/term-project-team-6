bc00aa02cc542594551802f7665ce52ed50cace9
Krishnan Ramakrishnan <Krishnan Ramakrishnan>
2016-11-28 23:36:26 -0800

    User registration form that stores relevant info in the database.

---
+++ db/user_reg.sql
@@ -0,0 +1,26 @@
+CREATE TABLE user_account 
+(
+ user_id VARCHAR(35)   NOT NULL PRIMARY KEY,
+ email_id VARCHAR(100) NOT NULL, 
+ first_name VARCHAR(70) NOT NULL,
+ last_name VARCHAR(70),
+ middle_name VARCHAR(70),
+ user_password VARCHAR(25),
+ created_time timestamp without time zone,
+ last_login_time timestamp without time zone  
+)
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+ 
